a062efc1976b8f5b820fe54e69de5d4d867b94bd
Josh Feldman <josh7up@gmail.com>
2015-03-25 16:52:51 -0400

    Added sample code for saving an assessment.

--- app/src/main/java/com/example/jaf50/survey/SurveyApplication.java
+++ app/src/main/java/com/example/jaf50/survey/SurveyApplication.java
@@ -4,6 +4,9 @@ import com.orm.SugarApp;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
+import java.util.Arrays;
+import java.util.Date;
+
 public class SurveyApplication extends SugarApp {
 
   private static final String APPLICATION_ID = "M8rcJZvA3poUvvJofyS4t5K0vtpHVLg3biM1NgVK";
@@ -15,8 +18,27 @@ public class SurveyApplication extends SugarApp {
     Parse.enableLocalDatastore(this);
     Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
-    ParseObject testObject = new ParseObject("TestObject");
-    testObject.put("haha", "LAWL!");
-    testObject.saveInBackground();
+    ParseObject survey = new ParseObject("Survey");
+    survey.put("name", "My Survey");
+
+    ParseObject participant = new ParseObject("Participant");
+    participant.put("assignedId", "123");
+
+    ParseObject assessmentResponse1 = new ParseObject("AssessmentResponse");
+    assessmentResponse1.put("responseId", "var1");
+    assessmentResponse1.put("responseDate", new Date());
+    assessmentResponse1.add("value", "val1");
+
+    ParseObject assessmentResponse2 = new ParseObject("AssessmentResponse");
+    assessmentResponse2.put("responseId", "var2");
+    assessmentResponse2.put("responseDate", new Date());
+    assessmentResponse2.add("value", "val2");
+
+    ParseObject assessment = new ParseObject("Assessment");
+    assessment.put("participant", participant);
+    assessment.put("survey", survey);
+    assessment.put("description", "Survey Description");
+    assessment.put("responses", Arrays.asList(assessmentResponse1, assessmentResponse2));
+    assessment.saveInBackground();
   }
 }
